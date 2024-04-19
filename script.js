aed80b48e161f7c025cb5bd3b0e9aac4483f6d37
Gabriel <gvqsilva@gmail.com>
2024-04-19 10:00:17 -0300

    Salvando o projeto

---
+++ script.js
@@ -0,0 +1,40 @@
+//VALIDAÇÃo DE LOGIN
+
+function validar(){
+
+    //declarando as variaveis
+    let usuario =document.getElementById("usuario").value;
+    let senha =document.getElementById("senha").value;
+
+    if(usuario == "Admin" && senha =='12345'){
+        window.open("dashboard.html")
+    }else{
+        alert("usuario e senha inválidos")
+    }
+
+}
+
+
+
+
+
+//CRIANDO UM SLIDESHOW AUTOMÁTICO  
+
+//declarando as variaveis
+
+let imagens=['./img/Carro1.jpg','./img/Carro2.jpg','./img/Carro3.jpg'];
+let index=0;
+let time =3000;
+
+function slideShow(){
+    document.getElementById("imgbanner").src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout('slideShow()', time);
+
+}
+
+slideShow();
